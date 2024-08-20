7051cfea7dc89a50a1e514a46eaf2f24b85a6ad0
vishalbigdata <vksrivastava6262@gmail.com>
2024-08-20 11:11:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern 6/pattern-6.py
@@ -0,0 +1,23 @@
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        # Code here
+        for j in range(N,0,-1):
+            for i in range(j):
+                print(i+1,end = ' ')
+            print() 
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input())
+        ob = Solution()
+        ob.printTriangle(N)
+# } Driver Code Ends
