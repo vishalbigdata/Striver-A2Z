6b35f8c107bcb1f0111e50c61345d218f48b326f
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-01 01:41:31 +0530

    Time: 44 ms (39.21%), Space: 16.4 MB (89.05%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,9 +1,12 @@
 class Solution:
-    def fib(self, n: int) -> int:
+    Memo = {}
+
+    def fib(self, n: int):
+        if n in self.Memo:
+            return self.Memo[n]
         if n == 0:
             return 0
         if n == 1:
             return 1
-        while True :
-            return Solution.fib(self,n-1) + Solution.fib(self,n-2)
-       
+        self.Memo[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.Memo[n]
