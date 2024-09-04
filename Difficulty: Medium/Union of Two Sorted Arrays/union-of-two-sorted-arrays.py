a4495cb7bd4e8a3c9dd7200a8ca3ae821c66cda0
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-05 00:25:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.py
+++ Difficulty: Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.py
@@ -2,32 +2,62 @@
 
 class Solution:
     
+    
+    '''
+    
+    arr1 = [1,1,2,3,4,5]
+    
+    
+    arr2 = [2,3,4,4,5,6]
+    
+    
+    '''
+    
+    
+    
+    
+    
+    
     #Function to return a list containing the union of the two arrays.
     def findUnion(self,arr1,arr2,n,m):
+        
         i, j = 0, 0  # Pointers
         union = []  # Union list
-    
-        while i < len(arr1) and j < len(arr2):
-            if arr1[i] <= arr2[j]:  # Case 1 and 2
+        
+        while i < len(arr1) and j < len(arr2) :
+            
+            # compare value of each array bw them
+            
+            if arr1[i] <= arr2[j]:
                 if len(union) == 0 or union[-1] != arr1[i]:
                     union.append(arr1[i])
-                i += 1
-            else:  # Case 3
+                i = i+1
+            else:
                 if len(union) == 0 or union[-1] != arr2[j]:
                     union.append(arr2[j])
-                j += 1
-    
-        while i < len(arr1):  # If any elements left in arr1
+                j = j+1
+        
+        while i < len (arr1):
             if union[-1] != arr1[i]:
                 union.append(arr1[i])
-            i += 1
-    
-        while j < len(arr2):  # If any elements left in arr2
+            i = i + 1
+            
+        while j < len (arr2):
             if union[-1] != arr2[j]:
                 union.append(arr2[j])
-            j += 1
-    
+            j = j + 1
+        
         return union
+                
+            
+                
+                    
+                
+            
+            
+            
+    
+        
                 
            
             
