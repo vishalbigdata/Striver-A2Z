35a6ceed9d784fd0c907c8763cc2b7f7b46b71cd
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-10 13:15:41 +0530

    Time: 251 ms (53.47%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,9 @@
-select max(salary) as SecondHighestSalary
-from Employee
-where salary not in (select max(salary) from Employee)
+-- select max(salary) as SecondHighestSalary
+-- from Employee
+-- where salary not in (select max(salary) from Employee)
+with cte as(
+select *,
+DENSE_RANK() over(order by salary desc) as r
+from Employee)
+select ifnull((select salary
+from cte where r = 2 limit 1 ), null) as SecondHighestSalary
