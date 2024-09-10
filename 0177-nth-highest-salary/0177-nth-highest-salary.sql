215ba4594309331026ff564bd054f6c61f9b8722
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-10 13:26:59 +0530

    Time: 388 ms (80.01%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,8 +1,21 @@
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+-- CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+-- BEGIN
+-- SET N = N-1;
+--   RETURN (
+--         select Distinct(salary) from employee
+--         order by salary desc limit 1 offset N
+--  );
+-- END
+
+CREATE FUNCTION getNthHighestSalary(N INT) 
+RETURNS INT
 BEGIN
-SET N = N-1;
   RETURN (
-        select Distinct(salary) from employee
-        order by salary desc limit 1 offset N
+    with cte as(
+        select *,dense_rank() over(order by salary desc) as rnk
+        from Employee)
+    select distinct ifnull(salary,null)
+    from cte
+    where rnk = N
  );
 END
