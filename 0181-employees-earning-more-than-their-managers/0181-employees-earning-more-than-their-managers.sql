39ff89bad0388c5c57548355bceb267068cbc0e4
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-10 14:22:24 +0530

    Time: 322 ms (91.07%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,6 +1,29 @@
 # Write your MySQL query statement below
+-- select e1.name as Employee 
+-- from Employee e1
+-- join Employee e2
+-- on e1.ManagerId = e2.id
+-- where e1.salary > e2.salary
+
+-- +----+-------+--------+-----------++----+-------+--------+-----------+
+-- | id | name  | salary | managerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | Null      |
+-- | 4  | Max   | 90000  | Null      |
+-- +----+-------+--------+-----------+
+
+-- | id | name  | salary | managerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | Null      |
+-- | 4  | Max   | 90000  | Null      |
+-- +----+-------+--------+-----------+
+
 select e1.name as Employee 
 from Employee e1
-join Employee e2
-on e1.ManagerId = e2.id
+join Employee e2 
+    on e1.managerId = e2.id
 where e1.salary > e2.salary
