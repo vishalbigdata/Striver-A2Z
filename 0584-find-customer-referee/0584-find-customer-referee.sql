d82671c9510667d3a30f3c5446c7033d3295c152
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-11 19:14:45 +0530

    Time: 372 ms (99%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
+-- select name
+-- from Customer
+-- where referee_id <> 2 or referee_id is NULL;
+
 select name
 from Customer
-where referee_id <> 2 or referee_id is NULL;
+-- where ifnull(referee_id , -1) <> 2
+where coalesce(referee_id,-1) <> 2
