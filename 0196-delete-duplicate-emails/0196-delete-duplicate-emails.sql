1580bd60d97bf322f89cd12e8f9122ac947b8d4b
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-11 18:33:18 +0530

    Time: 852 ms (52.3%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,7 +1,39 @@
-with my_cte as 
-            (select email ,min(id) as id_to_keep
-            from  Person group by Email
-            )
-            delete from Person
-            where Id not in ( select id_to_keep from my_cte )
+-- with my_cte as 
+--             (select email ,min(id) as id_to_keep
+--             from  Person group by Email
+--             )
+--             delete from Person
+--             where Id not in ( select id_to_keep from my_cte )
     
+-- 1 j 1 j  
+-- 1 j 2 b
+-- 1 j 3 j
+
+-- 2 b 1 j
+-- 2 b 2 b 
+-- 2 b 3 j
+
+-- 3 j 1 j
+-- 3 j 2 b
+-- 3 j 3 j
+
+
+delete p1.*
+from Person p1
+cross join  person p2
+where  p1.email = p2.email and p1.id > p2.id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
