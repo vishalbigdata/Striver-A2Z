440f5e331985f6217ae1761298e2c6157e23595e
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-15 18:10:17 +0530

    Time: 90 ms (13.1%), Space: 27.2 MB (27.09%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -1,35 +1,58 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-
+        
+        # get lineked_list lenght each seperately
         m = self.findLength(headA)
         n = self.findLength(headB)
-        fp  = headA
+
+        # now fixed one pointer to each Linked_list
+        fp = headA
         sp = headB
-        if m <= n:
+
+        # 
+        if m <= n :
             for i in range(n-m):
                 sp = sp.next
         else:
             for i in range(m-n):
                 fp = fp.next
-        
-        while fp is not None:
-            if fp == sp :
-                return fp # or sp
-            fp = fp.next
+
+        while fp != None or sp != None:
+            if fp == sp:
+                return fp or sp
             sp = sp.next
+            fp = fp.next
+        
         return None
+
 
     def findLength(self,head):
         length = 0
         curr = head
-        while(curr is not None):
+        while curr != None:
             curr = curr.next
             length = length + 1
         return length
+
+
+    #     m = self.findLength(headA)
+    #     n = self.findLength(headB)
+    #     fp  = headA
+    #     sp = headB
+    #     if m <= n: 
+    #         for i in range(n-m): sp = sp.next
+    #     else: 
+    #         for i in range(m-n): fp = fp.next
+    #     while fp is not None:
+    #         if fp == sp: return fp # or sp
+    #         fp = fp.next
+    #         sp = sp.next
+    #     return None
+    # def findLength(self,head):
+    #     length = 0
+    #     curr = head
+    #     while(curr is not None):
+    #         curr = curr.next
+    #         length = length + 1
+    #     return length
 
