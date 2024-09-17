c5e9d2c364f6e7a0d9875f0899fa90bcd9744a74
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-17 08:02:06 +0530

    Time: 29 ms (91.55%), Space: 16.6 MB (5.76%) - LeetHub

---
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.py
@@ -0,0 +1,34 @@
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+
+        l1 = s1.split(" ")
+        l2 = s2.split(" ")
+        res = []
+        
+        
+        d = dict()
+        for i in l1:
+            if i in d.keys():
+                d[i] = d[i] + 1
+            else:
+                d[i] = 1
+        for i in l2:
+            if i in d.keys():
+                d[i] = d[i] + 1
+            else:
+                d[i] = 1
+        for k,v in d.items():
+            if v == 1:
+                res.append(k)
+
+        return res
+            
+
+
+
+
+    
+       
+
+
+        
