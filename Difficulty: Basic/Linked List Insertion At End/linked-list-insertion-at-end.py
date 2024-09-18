c7c211d5d38bd3a8855daaa751c6315cfc3ff49c
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-18 18:23:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.py
@@ -0,0 +1,78 @@
+'''    
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+'''
+class Solution:
+    #Function to insert a node at the end of the linked list.
+    def insertAtEnd(self,head,x):
+        
+        nn = Node(x)
+        
+        if head is None:
+            head = nn
+        else:
+            curr = head
+            while(curr.next is not None):
+                curr = curr.next
+            curr.next = nn
+            
+        return head
+                
+      
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# Define the Node class for the linked list
+class Node:
+
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+
+
+# Function to print the linked list
+def printList(node):
+    while node:
+        print(node.data, end=" ")
+        node = node.next
+    print()
+
+
+#Position this line where user code will be pasted.
+
+# Main function
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
+
+    t = int(data[0].strip())
+    idx = 1
+
+    while t > 0:
+        arr = list(map(int, data[idx].strip().split()))
+        x = int(data[idx + 1].strip())
+        idx += 2
+
+        head = None
+        if arr:
+            head = Node(arr[0])
+            tail = head
+            for num in arr[1:]:
+                tail.next = Node(num)
+                tail = tail.next
+
+        ob = Solution()
+        ans = ob.insertAtEnd(head, x)
+        printList(ans)
+
+        t -= 1
+
+# } Driver Code Ends
