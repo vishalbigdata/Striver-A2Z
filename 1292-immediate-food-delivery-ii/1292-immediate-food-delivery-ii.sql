0f1cc6d3838dbcb360be512fc84279a6c970681e
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-22 03:45:35 +0530

    Time: 660 ms (37.4%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,20 +1,20 @@
-SELECT
-    ROUND((COUNT(
-        CASE WHEN d.order_date = d.customer_pref_delivery_date THEN 1 END) / COUNT(*)) * 100, 2)  immediate_percentage
-FROM Delivery d
-WHERE d.order_date = (
-    SELECT
-    MIN(order_date)
-    FROM Delivery
-    WHERE customer_id = d.customer_id
-    );
+-- SELECT
+--     ROUND((COUNT(
+--         CASE WHEN d.order_date = d.customer_pref_delivery_date THEN 1 END) / COUNT(*)) * 100, 2)  immediate_percentage
+-- FROM Delivery d
+-- WHERE d.order_date = (
+--     SELECT
+--     MIN(order_date)
+--     FROM Delivery
+--     WHERE customer_id = d.customer_id
+--     );
 
 -- OR
--- SELECT ROUND(AVG(temp.order_date=temp.customer_pref_delivery_date) * 100, 2) immediate_percentage
--- FROM (
---     SELECT *, RANK() OVER(partition by customer_id ORDER BY order_date) od
---     FROM Delivery) temp
--- WHERE temp.od = 1
+SELECT ROUND(AVG(temp.order_date=temp.customer_pref_delivery_date) * 100, 2) immediate_percentage
+FROM (
+    SELECT *, RANK() OVER(partition by customer_id ORDER BY order_date) od
+    FROM Delivery) temp
+WHERE temp.od = 1
 
 
 -- Select round(avg(if(d.order_date = d.customer_pref_delivery_date , 1 , 0 )) * 100 ,2) as immediate_percentage
