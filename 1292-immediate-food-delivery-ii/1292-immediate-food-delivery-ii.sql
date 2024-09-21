609805b53b7452b4978d52903e2d91bc5c0d5b9d
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-22 03:43:54 +0530

    Time: 1911 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,13 +1,13 @@
--- SELECT
---     ROUND((COUNT(
---         CASE WHEN d.order_date = d.customer_pref_delivery_date THEN 1 END) / COUNT(*)) * 100, 2)  immediate_percentage
--- FROM Delivery d
--- WHERE d.order_date = (
---     SELECT
---     MIN(order_date)
---     FROM Delivery
---     WHERE customer_id = d.customer_id
---     );
+SELECT
+    ROUND((COUNT(
+        CASE WHEN d.order_date = d.customer_pref_delivery_date THEN 1 END) / COUNT(*)) * 100, 2)  immediate_percentage
+FROM Delivery d
+WHERE d.order_date = (
+    SELECT
+    MIN(order_date)
+    FROM Delivery
+    WHERE customer_id = d.customer_id
+    );
 
 -- OR
 -- SELECT ROUND(AVG(temp.order_date=temp.customer_pref_delivery_date) * 100, 2) immediate_percentage
@@ -17,12 +17,12 @@
 -- WHERE temp.od = 1
 
 
-Select round(avg(if(d.order_date = d.customer_pref_delivery_date , 1 , 0 )) * 100 ,2) as immediate_percentage
-from Delivery d
-left join
-(select customer_id , min(order_date) as fod
-from Delivery
-group by customer_id) f
-on d.customer_id = f.customer_id
-where d.order_date = f.fod
+-- Select round(avg(if(d.order_date = d.customer_pref_delivery_date , 1 , 0 )) * 100 ,2) as immediate_percentage
+-- from Delivery d
+-- left join
+-- (select customer_id , min(order_date) as fod
+-- from Delivery
+-- group by customer_id) f
+-- on d.customer_id = f.customer_id
+-- where d.order_date = f.fod
 
