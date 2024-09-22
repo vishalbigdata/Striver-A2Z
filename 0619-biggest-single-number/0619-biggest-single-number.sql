5487566198ec5e924496910b19d84764eefd4153
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-22 05:30:55 +0530

    Time: 424 ms (30.44%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select max(num) as num from (select num , count(*)
+select max(num) as num from
+ (select num , count(*)
 from MyNumbers
 group by num
 having count(*) = 1) a
