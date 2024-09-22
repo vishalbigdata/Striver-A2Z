4b0d129c65c8dd0003aa84b1194ce933fda37c9b
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-23 01:38:32 +0530

    Time: 498 ms (71.9%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -12,11 +12,16 @@
 -- | 7  | 2   | null    null
 -- +----+-----+
 
-with cte as (
-    select *
-        ,lead(num,1) over () as nxt_1
-        ,lead(num,2) over () as nxt_2
-from Logs)
+-- with cte as (
+--     select *
+--         ,lead(num,1) over () as nxt_1
+--         ,lead(num,2) over () as nxt_2
+-- from Logs)
 
-select distinct num as  ConsecutiveNums from cte 
-where num = nxt_1 and num = nxt_2
+-- select distinct num as  ConsecutiveNums from cte 
+-- where num = nxt_1 and num = nxt_2
+
+select  distinct l1.num as ConsecutiveNums
+from Logs l1, Logs l2, Logs l3
+where l1.id = l2.id - 1 and l2.id = l3.id -1
+        and l1.num = l2.num and l2.num = l3.num
