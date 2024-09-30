0bb349eee66a097ca5e1dd22eae7893f031cc6a2
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-30 06:56:53 +0530

    Time: 49 ms (95.4%), Space: 17.8 MB (33.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,25 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)) :
-            for j in range(len(nums)):
-                if i == j: continue
-                if nums[i]  + nums[j]  == target:
-                    return [i,j]
+
+
+        viseted = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in viseted:
+                return [viseted[complement] , i]
+            viseted[num] = i
+        
+        return []
+
+
+        
+
+
+        # numMap = {}
+        # for i, num in enumerate(nums):
+        #     complement = target - num
+        #     if complement in numMap:
+        #         return [numMap[complement], i]
+        #     numMap[num] = i
+        # return []
+        
