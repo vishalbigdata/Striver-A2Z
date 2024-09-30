15c77302a433e2935709725ea47f08710a92d932
vishalbigdata <vksrivastava6262@gmail.com>
2024-10-01 03:28:27 +0530

    Time: 119 ms (86.51%), Space: 18.1 MB (64.43%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,34 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         nums.append(nums.pop(i))
+        
+        j = -1
+
+        # set j pointer to first zero
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                j = i
+                break
+        
+        # now j will update if element are ther 
+        # if now elemts
+
+        if j == -1:
+            return nums
+    
+
+        for i in range(j+1, len(nums)):
+            if nums[i] != 0:
+                # swap
+                nums[i], nums[j] = nums[j],nums[i]
+                j = j+1
+        
+        return nums
+        
+        
