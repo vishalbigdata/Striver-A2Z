70983edaa36f8a3d1a2a9abb44edb25eef12865b
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-30 13:16:00 +0530

    Time: 51 ms (92.19%), Space: 17.8 MB (33.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,24 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
+        # using to pointer approach
+        
+        # left = 0
+        # right = len(nums) -1
+
+        # while left <= right:
+        #     if (nums[left] + nums[right]) == target :
+        #         return [left,right]
+        #     elif (nums[left] + nums[right]) < target :
+        #         left = left + 1
+        #     elif (nums[left] + nums[right]) > target :
+        #         right = right - 1
+            
+        # it will not work for a case
+        #         nums = [3,2,4]
+
+
+        # using Hashing 
 
         viseted = {}
         for i, num in enumerate(nums):
