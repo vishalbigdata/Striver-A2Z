4432a476b5ed4cd856a6839982d7d484a75d1f9e
vishalbigdata <vksrivastava6262@gmail.com>
2024-09-30 17:33:11 +0530

    Time: 170 ms (60.56%), Space: 18.1 MB (26.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
+
+        n = len(nums)
+        from collections import Counter
+
+        counter = Counter(nums)
+
+        for num , count in counter.items():
+            if count > (n // 2) :
+                return num
+        
+        return -1
+
         # Bruteforce approach : 
         # n = len(nums)
         # for i in range(n):
@@ -15,22 +27,23 @@ class Solution:
         # Better  Approach
         # using Hashing 
 
-        n = len(nums)
-        dict = {}
-        if n < 2 :
-            return nums[0]
-        else:
-
-            for value in nums:
-                if value in dict.keys():
-                    dict[value] += 1
-                    if dict[value] > (n // 2) :
-                        return value
-                else:
-                    dict[value] = 1            
+        # n = len(nums)
+        # dict = {}
+        # if n < 2 :
+        #     return nums[0]
+        # else:
+        #     for value in nums:
+        #         if value in dict.keys():
+        #             dict[value] += 1
+        #             if dict[value] > (n // 2) :
+        #                 return value
+        #         else:
+        #             dict[value] = 1            
                 
             
 
+
+        
 
 
 
