06f49da384cecad4c9091c853d432a77f9e06ffd
vishalbigdata <vksrivastava6262@gmail.com>
2024-10-17 12:13:50 +0530

    Time: 720 ms (71.81%), Space: 27.5 MB (14.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,27 +1,37 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        #   With TC - O(N)
-
-        min_till_now = prices[0]
+        min_price = prices[0]
+        max_profit = 0
         profit = 0
 
-        for i in range(1,len(prices)):
-            min_till_now = min(min_till_now, prices[i])
-
-            profit = max(profit , prices[i] - min_till_now)
-
-        return profit
-
-        
-
-
-
+        for i in range(1, len (prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            else:
+                profit = prices[i] - min_price
+            
+            max_profit = max(profit, max_profit)
+            
+        return max_profit
 
 
+       
 
 
+        #   With TC - O(N)
 
+        # min_till_now = prices[0]
+        # profit = 0
+
+        # for i in range(1,len(prices)):
+        #     min_till_now = min(min_till_now, prices[i])
+
+        #     profit = max(profit , prices[i] - min_till_now)
+
+        # return profit
+
+    
         # Time Complexity - O(N*N)
         # max_profit = 0
         # for i in range(len(prices)):
