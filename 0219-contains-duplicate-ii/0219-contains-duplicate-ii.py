1614bec96ab131e13665e2956fa0ce9814453f6e
vishalbigdata <vksrivastava6262@gmail.com>
2024-10-19 13:36:16 +0530

    Time: 23 ms (100%), Space: 29.9 MB (56.71%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -9,17 +9,4 @@ class Solution:
             hashset[num] = i
         return False
 
-     
-
-           
-
-
-        # for  i in range(len(nums)-k):
-        #     for j in range(i+1, i+k+1):
-        #         if nums[i] == nums[j] :
-        #             return True
-        # return False
-
-
-
         
