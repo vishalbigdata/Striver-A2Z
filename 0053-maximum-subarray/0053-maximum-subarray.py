ad4bc61408f5d612ec5a069bf2cc3116c6f804c1
vishalbigdata <vksrivastava6262@gmail.com>
2024-10-19 15:32:21 +0530

    Time: 108 ms (99.85%), Space: 31.4 MB (22.88%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,18 +1,54 @@
 class Solution:
     import sys
     def maxSubArray(self, nums: List[int]) -> int:
-        maxi = -sys.maxsize - 1
 
-        summ = 0
+        max_sum = nums[0]
+        current_sum  = nums[0]
 
-        for i in range(len(nums)):
-            summ = summ + nums[i]
+        for i in range(1,len(nums)):
+            current_sum = max(nums[i],current_sum + nums[i])
+            max_sum = max(max_sum, current_sum)
 
-            if summ > maxi :
-                maxi = summ
-            
-            if summ < 0 :
-                summ = 0
+        return max_sum
+
+
+         # apprah 2 
+        # n = len(nums)
+        # max_sum = float('-inf')
+
+        # for i in range(n):
+        #     current_sum = 0
+        #     for j in range(i, n):
+        #         current_sum += nums[j]
+        #         max_sum = max(max_sum, current_sum)
+
+        # return max_sum
+
+        # aprach 1
+    
+        # n = len(nums)
+        # max_sum = float('-inf')  # Initialize to negative infinity
+
+        # for i in range(n):
+        #     for j in range(i, n):
+        #         current_sum = 0
+        #         for k in range(i, j + 1):
+        #             current_sum += nums[k]
+        #         max_sum = max(max_sum, current_sum)
+
+        # return max_sum
+
+        # optimal Apprach 3 :  kadens Algorithm 
         
-        return maxi
+        # maxi = -sys.maxsize - 1
+        # summ = 0
+        # for i in range(len(nums)):
+        #     summ = summ + nums[i]
+        #     if summ > maxi :
+        #         maxi = summ   
+        #     if summ < 0 :
+        #         summ = 0
         
+        # return maxi
+        
+    
