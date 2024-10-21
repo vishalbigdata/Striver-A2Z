7536b2e5b42df8d18a25c7dbf55a3438e6d4d6f0
vishalbigdata <vksrivastava6262@gmail.com>
2024-10-21 18:50:03 +0530

    Time: 7 ms (99.73%), Space: 17.2 MB (14.12%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,17 +1,37 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        
+        # optimal and better appoach using dynamic array
+        if not nums:
+            return 0
 
-        max_so_far = min_so_far = result = nums[0]
-
-        for i in range(1, len(nums)):
-            curr = nums[i]
-            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
-            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
-            max_so_far = temp_max
+        # Initialize max_product, min_product, and result with the first element
+        max_product = min_product = result = nums[0]
+    
+        # Iterate over the array starting from the second element
+        for num in nums[1:]:
+            # Calculate new candidates for max_product and min_product
+            temp_max = max(num, max_product * num, min_product * num)
+            temp_min = min(num, max_product * num, min_product * num)
             
-            result = max(result, max_so_far)
-
+            # Update max_product and min_product with the computed values
+            max_product = temp_max
+            min_product = temp_min
+            
+            # Update the result with the maximum of max_product
+            result = max(result, max_product)
+    
         return result
 
-            
+
+
+        # Bruteforce apprach 
+        # n = len(nums)
+        # max_product  = float('-inf')
+        
+        # for i in range(n):
+        #     current_product = 1
+        #     for j in range(i,n):
+        #         current_product *= nums[j]
+        #         max_product = max(current_product, max_product)
+
+        # return max_product
