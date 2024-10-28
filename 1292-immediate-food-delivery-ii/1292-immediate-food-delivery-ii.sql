ac0c1942c0c263af32bc1d76ea0d13f284530f42
vishalbigdata <vksrivastava6262@gmail.com>
2024-10-29 04:11:00 +0530

    Time: 681 ms (64.45%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -10,11 +10,11 @@
 --     );
 
 -- OR
-SELECT ROUND(AVG(temp.order_date=temp.customer_pref_delivery_date) * 100, 2) immediate_percentage
-FROM (
-    SELECT *, RANK() OVER(partition by customer_id ORDER BY order_date) od
-    FROM Delivery) temp
-WHERE temp.od = 1
+-- SELECT ROUND(AVG(temp.order_date=temp.customer_pref_delivery_date) * 100, 2) immediate_percentage
+-- FROM (
+--     SELECT *, RANK() OVER(partition by customer_id ORDER BY order_date) od
+--     FROM Delivery) temp
+-- WHERE temp.od = 1
 
 
 -- Select round(avg(if(d.order_date = d.customer_pref_delivery_date , 1 , 0 )) * 100 ,2) as immediate_percentage
@@ -25,4 +25,24 @@ WHERE temp.od = 1
 -- group by customer_id) f
 -- on d.customer_id = f.customer_id
 -- where d.order_date = f.fod
+select round(avg(if(d.order_date = d.customer_pref_delivery_date , 1 , 0 )) * 100 ,2) as immediate_percentage 
+from Delivery d
+left join(
+select customer_id , min(order_date) as fod
+from Delivery
+group by customer_id) f
+on d.customer_id = f.customer_id
+where d.order_date = f.fod
+
+
+
+
+
+
+
+
+
+
+
+
 
