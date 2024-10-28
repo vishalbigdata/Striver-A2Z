b50304ad5bef4cd76003d0c6166f08208d46d003
vishalbigdata <vksrivastava6262@gmail.com>
2024-10-29 01:45:13 +0530

    Time: 554 ms (38.68%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -11,8 +11,12 @@
 -- SUBDATE , we can use also
 
 
-select w1.id  as Id
-from weather w1
-left join weather w2
-on w1.recordDate - interval 1 day = w2.recordDate 
-where w1.temperature > w2.temperature 
+-- select w1.id  as Id
+-- from weather w1
+-- left join weather w2
+-- on w1.recordDate - interval 1 day = w2.recordDate 
+-- where w1.temperature > w2.temperature 
+select w2.id as id
+from Weather w1 join Weather w2
+    on datediff(w2.recordDate, w1.recordDate) = 1
+    and w2.temperature > w1.temperature
