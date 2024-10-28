80f33751ad0745e04b5524a70caabcd5e56002d0
vishalbigdata <vksrivastava6262@gmail.com>
2024-10-29 01:11:51 +0530

    Time: 717 ms (21.46%), Space: 0B (100%) - LeetHub

--- 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
@@ -1,4 +1,8 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
+-- select product_id 
+-- from Products
+-- where low_fats = 'Y' and recyclable = 'Y'
+
 select product_id 
-from Products
-where low_fats = 'Y' and recyclable = 'Y'
+from Products where
+low_fats = 'Y' and recyclable = 'Y'
