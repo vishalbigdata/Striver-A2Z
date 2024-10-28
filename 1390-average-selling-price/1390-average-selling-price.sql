9589fa41db98fc4d9e96026e93239a3c31b62667
vishalbigdata <vksrivastava6262@gmail.com>
2024-10-29 03:02:09 +0530

    Time: 887 ms (41.24%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -7,10 +7,30 @@
 -- group by p.product_id   
 
 
-select p.product_id, ifnull(round( sum(p.price * us.units)/ sum(us.units ) , 2) , 0) as average_price
-from Prices p
-left join UnitsSold us
-    on p.product_id = us.product_id
-    and us.purchase_date between p.start_date and p.end_date
+-- select p.product_id, ifnull(round( sum(p.price * us.units)/ sum(us.units ) , 2) , 0) as average_price
+-- from Prices p
+-- left join UnitsSold us
+--     on p.product_id = us.product_id
+--     and us.purchase_date between p.start_date and p.end_date
+-- group by p.product_id
+
+
+
+
+
+select 
+     p.product_id,ifnull( round(sum(us.units * p.price) / sum( us.units), 2 ), 0 )as average_price
+from 
+    Prices p
+left join 
+    UnitsSold us
+on
+    p.product_id = us.product_id 
+            and
+    us.purchase_date between p.start_date and p.end_date
 group by p.product_id
+
+
+
+
 
